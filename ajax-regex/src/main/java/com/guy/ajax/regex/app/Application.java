c77a606ae8b6aaf1abde3718cf44994b51f8b414
Guy Turner <guturner@users.noreply.github.com>
2016-05-29 19:01:02 -0400

    Spring Boot Thymeleaf config

---
+++ ajax-regex/src/main/java/com/guy/ajax/regex/app/Application.java
@@ -0,0 +1,15 @@
+package com.guy.ajax.regex.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.guy.ajax.regex.controllers"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
